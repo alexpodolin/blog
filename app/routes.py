ef72d7740016622fff3289c0eb57c7f46ed64e50
alexpodolin <alex.podolin@gmail.com>
2018-07-31 10:22:40 +0300

    second chapter

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hellow World!'
+    user = {'username': 'Alex'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }, 
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
