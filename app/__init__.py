5540b54ca72be6d17c1a1088f72ce74df5d5b828
alexpodolin <alex.podolin@gmail.com>
2018-09-14 14:11:24 +0300

    tenth chapter

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
-import logging
-import os
-from logging.handlers import SMTPHandler, RotatingFileHandler
-
+from flask_mail import Mail
+from config import Config
 
 app = Flask(__name__)
 
@@ -19,7 +18,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models, errors
+mail = Mail(app)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -45,3 +45,5 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+from app import routes, models, errors
